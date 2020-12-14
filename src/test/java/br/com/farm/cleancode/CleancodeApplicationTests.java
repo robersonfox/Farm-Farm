3380b6833139accf6668c5714954bdf626e50df6
Roberson Carlos <robersonfox@gmail.com>
2020-12-14 17:47:24 -0300

    initial commit

---
+++ src/test/java/br/com/farm/cleancode/CleancodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.farm.cleancode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleancodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
