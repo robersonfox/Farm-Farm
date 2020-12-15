582825c0db819bbcf7e6b913d8701f90f132c4b1
Roberson Carlos <robersonfox@gmail.com>
2020-12-15 00:19:17 -0300

    .

---
+++ src/main/java/br/com/cleancode/CleancodeApplication.java
@@ -0,0 +1,24 @@
+package br.com.cleancode;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class CleancodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleancodeApplication.class, args);
+
+	}
+	
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
+}
